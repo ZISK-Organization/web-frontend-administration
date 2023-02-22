a66c57fb77606f3dad30403ba551b5d47e43ff26
Jakub Šťastný <xstastnyj@gmail.com>
2023-02-22 05:18:09 +0100

    Update Users.tsx

--- src/Pages/Users/Users.tsx
+++ src/Pages/Users/Users.tsx
@@ -53,9 +53,6 @@ export default function Users() {
       </Table>
       <br />
       <br />
-      {
-        // TODO - delete mjancovicova@gmail.com next year. This year, I promised to invite her for all our actions because of the help with the propagations.  
-      }
       <Button href={`mailto:${users.map((u) => u.contactMail).join(",")}`} color="primary" variant="contained">
         Odeslat Email všem řešitelům
       </Button>
